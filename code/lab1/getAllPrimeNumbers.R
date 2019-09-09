6a3f103224afc4ce5344e2b2f63692f641f129d6
Mahasen Dehideniya <mahasendehideniya@gmail.com>
2019-09-09 05:50:07 +0530

    Lab session 1

---
+++ code/lab1/getAllPrimeNumbers.R
@@ -0,0 +1,5 @@
+getAllPrimeNumbers <- function(x)
+{
+  # write the function body to find all prime numbers upto a given number x
+  
+}
---
+++ code/lab1/getFactorial.R
@@ -0,0 +1,4 @@
+getFactorial <- function(x)
+{
+  # write the function body to find the factorial of a  given number x
+}
